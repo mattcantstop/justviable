494c79def3f7e73976de3a78fc4e59291273c815
Matthew Duff <matthewlduff@gmail.com>
2012-01-19 15:44:54 -0700

    created simple post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,20 @@ class PostsController < ApplicationController
   end
   
   def new
-    
+    @post = Post.new(params[:post])
   end
   
   def create
-    
+    @post = Post.new(params[:post])
+      if @post.save
+        redirect_to posts_url, notice: "Successfully Saved"
+      else
+        redirect_to new_post_url, notice: "Something Went Wrong, Let's Try That Again."
+      end
+  end
+  
+  def index
+    @posts = Post.all(params[:post])
   end
   
 end
