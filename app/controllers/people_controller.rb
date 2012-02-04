90780cd85c751b191a8daed0a1da363e11f49e45
Matthew Duff <matthewlduff@gmail.com>
2012-02-04 13:11:21 -0700

    Adding Users

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,15 +1,19 @@
 class PeopleController < ApplicationController
 
 	def index
-
+		@person = Person.all(params[:id])
 	end
 
 	def new
-
+		@person = Person.new(params[:person])
 	end
 
 	def create
-
+		@person = Person.new(params[:id])
+		if @person.save
+			redirect_to people_url, notice: "Successfully Added a Person"
+		else
+			redirect_to new_person_url, notice: "Please Try Again"
 	end
 
 	def update
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < ApplicationController
+
+	def new
+		@session = Session.new(params[:id])
+	end
+
+	def destroy
+
+	end
+
 end
